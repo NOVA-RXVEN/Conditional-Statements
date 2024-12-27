5dd938255e229f5b5f397a46d3d55f9ae4743d27
Liban Tahsin <tahsin.liban0927@gmail.com>
2024-12-28 01:39:43 +0600

    ACP

---
+++ ACP.py
@@ -0,0 +1,12 @@
+print("\033c")
+
+temp = float(input("Enter the Temperature of your City: "))
+
+if temp < 20:
+    print("My Dear; You should wear Warm clothes!")
+    
+elif temp == 20:
+    print("WEAR WHATEVER YOU WANT MR!")
+    
+else:
+    print("My Dear; You should wear Light or Thin clothes!")
--- Odd  Even.py
+++ Odd  Even.py
@@ -1,5 +1,5 @@
 print("\033c")
-num = int(input("Please Enter a Number: "))
+num = int(input("Please Enter a Number:"))
 
 if num % 2 ==0:
     print(f"The number {num} is EVEN")
