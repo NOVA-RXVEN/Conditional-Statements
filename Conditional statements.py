01aee989d725a6f4732c3fe9558160ea04487f98
Liban Tahsin <tahsin.liban0927@gmail.com>
2024-12-27 18:37:35 +0600

    first commit

---
+++ Conditional statements.py
@@ -0,0 +1,11 @@
+age = 19
+
+if age > 18:
+    print("You are eligible to have NID card")
+    print("I am inside if statement")
+    
+else:
+    print("You are still a child")    
+    print("I am inside Else statement")
+
+print("I am outside of all the statements")    
---
+++ Odd  Even.py
@@ -0,0 +1,8 @@
+print("\033c")
+num = int(input("Please Enter a Number: "))
+
+if num % 2 ==0:
+    print(f"The number {num} is EVEN")
+    
+else:
+    print(f"The number {num} is ODD")    
